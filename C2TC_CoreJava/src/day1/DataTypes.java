36a93d64606550d7dba66106bded1a1bedba6e20
Kalaivani269 <kalaivaniofficial26@gmail.com>
2025-07-21 20:29:49 +0530

    Signed-off-by: Kalaivani269 <kalaivaniofficial26@gmail.com>

---
+++ C2TC_CoreJava/src/day1/DataTypes.java
@@ -0,0 +1,27 @@
+package day1;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		
+		int a = 36/6;
+		float b= 3/2f;// float div
+		double c = 15/7.2;
+		double d =  71/6d; // double div
+		
+		float e= 21/2.1f;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+		int std= 499;
+		
+		System.out.println(a*std);
+		
+		
+		
+	}
+
+}
