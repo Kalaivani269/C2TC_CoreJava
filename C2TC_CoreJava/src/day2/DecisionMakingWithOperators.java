d5a213c9b2c04c917707a58e612225b25a4bbebf
bet630 <vanik9411@gmail.com>
2025-07-21 22:52:23 +0530

    Decision making with operators

---
+++ C2TC_CoreJava/src/day2/DecisionMakingWithOperators.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class DecisionMakingWithOperators {
+
+public static void main(String[] args) {
+	
+	int a=12;
+	int b=9;
+	int c=30;
+	int d=16;
+	if(a>b) {
+		System.out.println("yes");
+	}
+	else {
+		System.out.println("No");
+	}
+	if(c==d) {
+		System.out.println("Equal");
+	}
+	else {
+		System.out.println("Not equal");
+	}
+}
+}
