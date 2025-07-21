2275bbaf66896cb560d08fcff7002e6791a85549
bet630 <vanik9411@gmail.com>
2025-07-21 22:40:19 +0530

    break statement

---
+++ C2TC_CoreJava/src/day2/BreakDemo.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class BreakDemo {
+	public static void main(String[] args) {
+		int a=2;
+		int b=8;
+		int c;
+		for(int i=0;i<10;i++) {
+			c= a+b;
+			if(i==7)
+				break;
+		System.out.println(i);
+		}
+	}
+
+}
