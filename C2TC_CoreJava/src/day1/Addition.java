f027c7aa398a98306744c9df272cdae21b34bb75
Kalaivani269 <kalaivaniofficial26@gmail.com>
2025-07-21 16:05:30 +0530

    Addition
    Signed-off-by: bet630 <vanik9411@gmail.com>
    Signed-off-by: Kalaivani269 <kalaivaniofficial26@gmail.com>

---
+++ C2TC_CoreJava/src/day1/Addition.java
@@ -0,0 +1,18 @@
+package day1;
+import java.util.Scanner;
+public class Addition {
+	public static void main(String[] args) {
+		
+		int a;
+		int b;
+		int c;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the values:");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c=a+b;
+		System.out.println("Addition of"+" a="+ a+" b="+ b+" is"+ c);
+		
+	}
+
+}
