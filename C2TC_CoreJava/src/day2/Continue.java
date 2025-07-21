d166c64ba072f1aada065548ab8babf4bffad553
bet630 <vanik9411@gmail.com>
2025-07-21 22:48:07 +0530

    continue statement

---
+++ C2TC_CoreJava/src/day2/Continue.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Continue {
+	public static void main(String[] args) {
+		int n=20;
+		int i;
+		for( i=0;i<n;i++) {
+			if(n%2==0)
+				continue;
+		}
+		System.out.println(n);
+		
+	}
+
+}
