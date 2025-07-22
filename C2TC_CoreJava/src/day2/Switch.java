d27bbeedf2dbef1ebf809cce2392dddb240809a7
bet630 <vanik9411@gmail.com>
2025-07-22 09:47:19 +0530

    switch statement

---
+++ C2TC_CoreJava/src/day2/Switch.java
@@ -0,0 +1,31 @@
+package day2;
+
+public class Switch {
+	public static void main(String[] args) {
+		
+		int a=5;
+		switch(a)
+		{
+		case 1:
+			System.out.println("odd");
+			break;
+		
+		case 2:
+			System.out.println("even");
+			break;
+		
+		case 3:
+			System.out.println("odd");
+			break;
+		case 4:
+			System.out.println("even");
+			break;
+		case 5:
+		System.out.println("odd");
+		break;
+		
+		}
+		
+	}
+
+}
