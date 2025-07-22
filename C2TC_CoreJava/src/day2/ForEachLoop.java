be1c9de6c497740ff176473bc6204b9fdf12b1d0
bet630 <vanik9411@gmail.com>
2025-07-22 07:58:54 +0530

    for each loop

--- C2TC_CoreJava/src/day2/ForEachLoop.java
+++ C2TC_CoreJava/src/day2/ForEachLoop.java
@@ -1,5 +1,25 @@
 package day2;
 
 public class ForEachLoop {
-
+	public static void main(String[] args) {
+        // int type
+        int arr[]={10,20,30,40,50,60};
+        for(int array:arr){
+            System.out.print(array+" ");
+        }
+        System.out.println(" ");
+        // character type
+        char ch[] = {'a','b','c','d'};
+        for(char alph:ch){
+            System.out.print(alph);
+        }
+        
+        System.out.println(" ");
+        
+        // String type
+        String str[]={"java","programming"};
+        for(String s:str){
+            System.out.print(s);
+        }
+        }
 }
