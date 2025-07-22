4579301b634b24f1c74f7748d59e1206f6e0cbba
bet630 <vanik9411@gmail.com>
2025-07-22 08:38:10 +0530

    If statement example

---
+++ C2TC_CoreJava/src/day2/If.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class If {
+
+	public static void main(String[] args) {
+		int n=16;
+		if(n==10) {
+			System.out.println(n);
+		}
+		else {
+			System.out.println("no");
+		}
+	}
+}
