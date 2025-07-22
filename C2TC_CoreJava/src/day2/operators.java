ffe705f63ed69bbcb2c8c1f49f10692500f96f68
bet630 <vanik9411@gmail.com>
2025-07-22 09:29:56 +0530

    different types of operators

---
+++ C2TC_CoreJava/src/day2/operators.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class operators {
+	public static void main(String[] args) {
+		int a=12;
+		int b= 10;
+		System.out.println(a+b);
+		System.out.println(a/b);
+		System.out.println(a*b);
+		
+		//increment and decrement
+		
+		System.out.println(++a + b-- + a++);
+		
+		// ternary
+		
+		a=(a==12) ? 1 : 0;
+		
+		System.out.println(a);
+		 
+	}
+}
