15997e235b47b6769999a1fa751ece18f36dbad3
bet630 <vanik9411@gmail.com>
2025-07-24 15:31:37 +0530

    Constructor

--- C2TC_CoreJava/src/daythree/constructor/customer.java
+++ C2TC_CoreJava/src/daythree/constructor/customer.java
@@ -1,5 +1,58 @@
 package daythree.constructor;
 
 public class customer {
+	
+		int customerId;
+		String customerName;
+		String customerCity;
+		
+		//default
+		public customer() {
+			
+			
+		}
+		
+		//parametrised
+		public customer(int customerId,String customerName,String customerCity) {
+			
+			this.customerId=customerId;
+			this.customerName=customerName;
+			this.customerCity=customerCity;	
+		}
 
-}
+		public int getCustomerId() {
+			return customerId;
+		}
+
+		public void setCustomerId(int customerId) {
+			this.customerId = customerId;
+		}
+
+		public String getCustomerName() {
+			return customerName;
+		}
+
+		public void setCustomerName(String customerName) {
+			this.customerName = customerName;
+		}
+
+		public String getCustomerCity() {
+			return customerCity;
+		}
+
+		public void setCustomerCity(String customerCity) {
+			this.customerCity = customerCity;
+		}
+
+		@Override
+		public String toString() {
+			return "customer [customerId=" + customerId + ", customerName=" + customerName + ", customerCity="
+					+ customerCity + "]";
+		}
+		
+		
+
+	}
+
+
+
--- C2TC_CoreJava/src/daythree/constructor/customerDemo.java
+++ C2TC_CoreJava/src/daythree/constructor/customerDemo.java
@@ -1,5 +1,43 @@
 package daythree.constructor;
-
+import java.util.Scanner;
 public class customerDemo {
-
+	
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Enter the Id");
+		int id=sc.nextInt();
+		
+		sc.nextLine();
+		System.out.println("Enter the name");
+		String name=sc.nextLine();
+		
+		System.out.println("Enter the City");
+		String city=sc.nextLine();
+		
+		customer c1= new customer();
+		c1.setCustomerId(id);
+		c1.setCustomerName(name);
+		c1.setCustomerCity(city);
+		System.out.println(" ");
+		
+		System.out.println(c1);
+		
+		System.out.println(" ");
+		
+		customer c2 = new customer();
+		
+		System.out.println("Enter the Id");
+		 id=sc.nextInt();
+		 
+		 sc.nextLine();
+		System.out.println("Enter the name");
+		 name=sc.nextLine();
+		
+		System.out.println("Enter the City");
+		 city=sc.nextLine();
+		
+		System.out.println(c2);	
+		}
 }
