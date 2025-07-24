82d6885a0d1ce59e5a44b52765c06dfeeea96dc7
bet630 <vanik9411@gmail.com>
2025-07-24 23:54:32 +0530

    inheritance

---
+++ C2TC_CoreJava/src/com/tnsif/dayfour/secondPackage/Executer.java
@@ -0,0 +1,21 @@
+package com.tnsif.dayfour.secondPackage;
+import com.tnsif.dayfour.firstpackage.Base;
+
+public class Executer {
+
+	public static void main(String[] args) {
+		
+		Base base=new Base();
+		
+		base.varPublic=56;
+		base.methodPublic();
+		
+//		base.varProtected=88;
+//		base.methodProtected();
+		
+		
+
+
+	}
+
+}
