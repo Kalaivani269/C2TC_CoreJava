1c8196ef0bb200c4bd54d805a6509aef009f4332
bet630 <vanik9411@gmail.com>
2025-07-30 18:51:55 +0530

    Nested Interface

---
+++ C2TC_CoreJava/src/com/tnsif/dayeigth/Nestedinterface/MyInterface.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayeigth.Nestedinterface;
+
+public interface MyInterface {
+	 
+	void calculateArea();
+	    interface MyInnerInterface {
+	       int  id = 20;
+	       void print();     
+	    }
+
+}
---
+++ C2TC_CoreJava/src/com/tnsif/dayeigth/Nestedinterface/NestedInterfaceClass.java
@@ -0,0 +1,12 @@
+package com.tnsif.dayeigth.Nestedinterface;
+
+public class NestedInterfaceClass implements MyInterface.MyInnerInterface{
+
+	@Override
+	public void print() {
+		 System.out.println("Print method of nested interface");
+	}
+
+	
+
+}
