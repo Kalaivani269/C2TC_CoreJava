b80e027bff4f30e1b45e5874d3078d5550639e63
bet630 <vanik9411@gmail.com>
2025-07-30 17:25:08 +0530

    Extended Interface

---
+++ C2TC_CoreJava/src/com/tnsif/dayeight/extendedInterfaces/ChildInterface.java
@@ -0,0 +1,7 @@
+package com.tnsif.dayeight.extendedInterfaces;
+	
+	public interface ChildInterface extends InterfaceOne {
+		
+		void show();
+	}
+
---
+++ C2TC_CoreJava/src/com/tnsif/dayeight/extendedInterfaces/InterfaceOne.java
@@ -0,0 +1,6 @@
+package com.tnsif.dayeight.extendedInterfaces;
+
+public interface InterfaceOne {
+
+	  void print();  
+}
